93ad899056fe9a52068a24caab2edbf521c9db9a
osqat <osqatt@gmail.com>
2026-06-07 12:31:15 +0300

    Add -beta display suffix

--- Plugin.cs
+++ Plugin.cs
@@ -6,12 +6,12 @@ using System;
 using System.IO;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyVersion("0.10.0")]
+[assembly: System.Reflection.AssemblyVersion("0.10.1")]
 [assembly: Cinnamon.AutoUpdate("Osqat/Cinnamon-")]
 
 namespace Cinnamon
 {
-    [BepInPlugin("com.osqat.cinnamon", "Cinnamon", "0.10.0" + PreRelease)]
+    [BepInPlugin("com.osqat.cinnamon", "Cinnamon", "0.10.1")]
     public class Plugin : BaseUnityPlugin
     {
         internal const string PreRelease = "-beta"; // set to "" for stable releases
